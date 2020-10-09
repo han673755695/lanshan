f87ea85a830d2f84c26270a99812de9e37bd5f89
hanyd <hyd@ZYWX76803>
2020-10-09 17:17:41 +0800

    强制提交

--- lanshan/src/main/java/com/han/lanshan/generator/service/IGeneratorService.java
+++
@@ -1,9 +0,0 @@
-package com.han.lanshan.generator.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface IGeneratorService {
-
-	List<Map<String,String>> selectColumnsByTableName(String tableName);
-}
